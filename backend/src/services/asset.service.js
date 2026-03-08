3bbd619f75b1c3442bf3b6d567d27c7b9b9823dd
Nelna-Group <itdepartment@nelna.lk>
2026-03-08 08:46:55 +0000

    delete

--- backend/src/services/asset.service.js
+++
@@ -1,9 +0,0 @@
-const BaseService = require('./base.service');
-
-class AssetService extends BaseService {
-  constructor() {
-    super('assets');
-  }
-}
-
-module.exports = new AssetService();
--- backend/src/services/user.service.js
+++
@@ -1,9 +0,0 @@
-const BaseService = require('./base.service');
-
-class UserService extends BaseService {
-  constructor() {
-    super('users');
-  }
-}
-
-module.exports = new UserService();
